29b4a5a440b9b3d95008a018d60ff7ce59f5b131
Pierre Nel <pierre.nel@gmail.com>
2016-07-05 20:13:47 +0100

    + sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
-var gulp = require('gulp');
-var fs = require("fs");
-var browserify = require("browserify");
-var babelify = require("babelify");
-var source = require('vinyl-source-stream');
-var gutil = require('gulp-util');
+var gulp = require('gulp'),
+		fs = require("fs"),
+		browserify = require("browserify"),
+		babelify = require("babelify"),
+		source = require('vinyl-source-stream'),
+		gutil = require('gulp-util'),
+		sass = require('gulp-sass'),
+  	concat = require('gulp-concat'),
+  	prefix = require('gulp-autoprefixer'),
+  	minify = require('gulp-minify-css');
 
 // Lets bring es6 to es5 with this.
 // Babel - converts ES6 code to ES5 - however it doesn't handle imports.
-// Browserify - crawls your code for dependencies and packages them up 
+// Browserify - crawls your code for dependencies and packages them up
 // into one file. can have plugins.
-// Babelify - a babel plugin for browserify, to make browserify 
+// Babelify - a babel plugin for browserify, to make browserify
 // handle es6 including imports.
 gulp.task('es6', function() {
 	browserify({ debug: true })
@@ -21,8 +25,26 @@ gulp.task('es6', function() {
     	.pipe(gulp.dest('./'));
 });
 
-gulp.task('watch',function() {
-	gulp.watch(['./app/**/*.js'],['es6'])
+gulp.task('styles', function() {
+  return gulp.src(['./app/scss/master.scss'])
+    .pipe(sass({
+      sourceComments: 'map'
+    }))
+    .on('error', function(err) {
+      console.log(err.toString());
+      this.emit('end');
+    })
+    .pipe(prefix({
+      browsers: ['> 1%', 'last 3 versions', 'ie 8']
+    }))
+    .pipe(concat('styles.css'))
+    .pipe(minify())
+    .pipe(gulp.dest('./'));
 });
- 
-gulp.task('default', ['es6','watch']);
+
+gulp.task('watch',function() {
+	gulp.watch(['./app/**/*.js'],['es6']);
+	gulp.watch(['./app/scss/*.scss','!./scss/ie8.scss','!./scss/ie9.scss'], ['styles']);
+});
+
+gulp.task('default', ['styles','es6','watch']);
